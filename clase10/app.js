8789e9e559b9dde14cdc1141423410b1455fe130
Andrey Bejarano <andreybejarano@gmail.com>
2022-06-17 00:04:46 -0300

    clase 10

---
+++ clase10/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'));
+});
+
+app.get('/babbage', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/babbage.html'));
+});
+
+app.get('/berners-lee', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/berners-lee.html'));
+});
+
+app.get('/clarke', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/clarke.html'));
+});
+
+app.get('/hamilton', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/hamilton.html'));
+});
+
+app.get('/hopper', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/hopper.html'));
+});
+
+app.get('/lovelace', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/lovelace.html'));
+});
+
+app.get('/turing', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/turing.html'));
+});
+
+app.listen(3030, () => {
+    console.log('Servidor iniciado en: http://localhost:3030');
+});
