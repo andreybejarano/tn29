56eb1acfaa880b32f8f78128e7a0cf2ab9e06a3a
Andrey Bejarano <andreybejarano@gmail.com>
2022-07-12 21:03:13 -0300

    clase 16

---
+++ clase16/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
